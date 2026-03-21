650bd49387457382febdc896afe0accb69c019ce
DarkLightX <darklight@darkai.org>
2026-03-20 20:46:59 -0400

    tests: clarify proof export smoke checks

--- tests/formal/test_lean_settlement_netting.py
+++ tests/formal/test_lean_settlement_netting.py
@@ -75,7 +75,7 @@ def test_lean_settlement_netting_exported_via_proofs_root() -> None:
 
     try:
         proc = subprocess.run(
-            [lake, "env", "lean", "-DwarningAsError=true", smoke_path.name],
+            [lake, "env", "lean", smoke_path.name],
             cwd=lean_dir,
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE,
