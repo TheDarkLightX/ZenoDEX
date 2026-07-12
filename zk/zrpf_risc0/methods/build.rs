c194b63b5d078a8dcba5270586478e0154863dfa
DarkLightX <darklight@darkai.org>
2026-07-12 14:31:04 -0400

    zk: add V5 value aggregate level-two guest

--- zk/zrpf_risc0/methods/build.rs
+++ zk/zrpf_risc0/methods/build.rs
@@ -31,6 +31,8 @@ pub const ZENODEX_ZRPF_RISC0_SPOT_VALUE_LEAF_V4_ELF: &[u8] = &[];
 pub const ZENODEX_ZRPF_RISC0_SPOT_VALUE_LEAF_V4_ID: [u32; 8] = [0; 8];
 pub const ZENODEX_ZRPF_RISC0_VALUE_AGGREGATE_L1_ELF: &[u8] = &[];
 pub const ZENODEX_ZRPF_RISC0_VALUE_AGGREGATE_L1_ID: [u32; 8] = [0; 8];
+pub const ZENODEX_ZRPF_RISC0_VALUE_AGGREGATE_L2_ELF: &[u8] = &[];
+pub const ZENODEX_ZRPF_RISC0_VALUE_AGGREGATE_L2_ID: [u32; 8] = [0; 8];
 "#;
     fs::write(methods_rs, stub).expect("write placeholder methods.rs");
 }
